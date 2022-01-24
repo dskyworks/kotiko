fc7af46b177ffb87093b6d50408e432f7182ebcd
Vladislav Dribinsky <dskyworks@yandex.ru>
2022-01-24 23:06:45 +0300

    Подключает swiperjs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import "./index.pug";
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
 import "./styles/index.scss";
+
+Swiper.use([Navigation]);
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
