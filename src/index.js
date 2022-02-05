113f8cc786de8c7647903bb3b6bb997c5e213cf8
Vladislav Dribinsky <dskyworks@yandex.ru>
2022-02-05 11:57:49 +0300

    Добавляет галерею

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./index.pug";
-import Swiper, { Navigation } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "./styles/index.scss";
 
 const roomCardSwiper = new Swiper('.swiper-room-card', {
@@ -13,5 +14,41 @@ const roomCardSwiper = new Swiper('.swiper-room-card', {
   navigation: {
     nextEl: '.swiper-room-card-button-next',
     prevEl: '.swiper-room-card-button-prev',
+  },
+});
+
+// Gallery
+const galleryBigSlider = new Swiper(".swiper-gallery-big", {
+  modules: [Navigation, Pagination],
+  loop: true,
+  allowTouchMove: true,
+  slidesPerView: 'auto',
+  speed: 200,
+  breakpoints: {
+    800: {
+      speed: 0,
+      allowTouchMove: false,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-gallery-button-next",
+    prevEl: ".swiper-gallery-button-prev",
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+});
+
+const gallerySmallSlider = new Swiper(".swiper-gallery-small", {
+  modules: [Navigation],
+  loop: true,
+  speed: 0,
+  allowTouchMove: false,
+  spaceBetween: 30,
+  slidesPerView: 2,
+  navigation: {
+    nextEl: ".swiper-gallery-button-next",
+    prevEl: ".swiper-gallery-button-prev",
   },
 });
