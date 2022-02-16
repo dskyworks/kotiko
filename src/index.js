27683ab695138ef885c9e08fee437f016dcbcf80
Vladislav Dribinsky <dskyworks@yandex.ru>
2022-02-16 21:06:46 +0300

    Добавляет секцию "Адрес и контакты"

--- src/index.js
+++ src/index.js
@@ -52,3 +52,72 @@ const gallerySmallSlider = new Swiper(".swiper-gallery-small", {
     prevEl: ".swiper-gallery-button-prev",
   },
 });
+
+// Contacts address copy to clipboard
+
+const contactsStreetToClipboardButton = document.querySelector(
+  ".contacts__street-to-clipboard"
+);
+const contactsStreet = document.querySelector(".contacts__street");
+
+contactsStreetToClipboardButton.addEventListener("click", () => {
+  window.Clipboard = (function (window, document, navigator) {
+    let textArea, copy;
+
+    function isOS() {
+      return navigator.userAgent.match(/ipad|iphone/i);
+    }
+
+    function createTextArea(text) {
+      textArea = document.createElement("textArea");
+      textArea.value = text;
+      document.body.appendChild(textArea);
+    }
+
+    function selectText() {
+      let range, selection;
+
+      if (isOS()) {
+        range = document.createRange();
+        range.selectNodeContents(textArea);
+        selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        textArea.setSelectionRange(0, 999999);
+      } else {
+        textArea.select();
+      }
+    }
+
+    function copyToClipboard() {
+      document.execCommand("copy");
+      document.body.removeChild(textArea);
+    }
+
+    copy = function (text) {
+      createTextArea(text);
+      selectText();
+      copyToClipboard();
+    };
+
+    return {
+      copy: copy,
+    };
+  })(window, document, navigator);
+
+  Clipboard.copy(contactsStreet.innerText);
+
+  contactsStreetToClipboardButton.classList.add(
+    "contacts__street-to-clipboard--copied"
+  );
+  contactsStreetToClipboardButton.disabled = true;
+  const buttonText = contactsStreetToClipboardButton.innerText;
+  contactsStreetToClipboardButton.textContent = "Скопировано";
+  setTimeout(() => {
+    contactsStreetToClipboardButton.classList.remove(
+      "contacts__street-to-clipboard--copied"
+    );
+    contactsStreetToClipboardButton.textContent = buttonText;
+    contactsStreetToClipboardButton.disabled = false;
+  }, 2000);
+});
